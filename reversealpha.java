c9114689c355ba88b797789c8bb53b94b88153d6
venu7273 <109106057+venu7273@users.noreply.github.com>
2022-09-23 10:59:29 +0530

    Add files via upload

---
+++ reversealpha.java
@@ -0,0 +1,26 @@
+import java.util.*; 
+import java.util.Scanner;
+public class reversealpha { 
+  public static void main(String[] args) { 
+  Scanner sc=new Scanner(system.in); 
+  System.out.println("Enter String:"); 
+  String str=sc.nextLine(); 
+  String[] str1=str.split("\\s"); 
+  for(int i=0;i<str1.length;i++) 
+  { 
+  for(int j=i+1;j<str1.length;j++) 
+  { 
+  if(str1[i].length()>str1[j].length()) 
+  { 
+  String temp= str1[i]; 
+  str1[i]=str1[j]; 
+  str1[j]=temp; 
+  } 
+  } 
+   } 
+  for(int i=0;i<str1.length;i++) 
+  { 
+  System.out.print(str1[i]+" ");  
+  } 
+  } 
+}
