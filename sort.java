3cd465aa882ab17432997b7534c18c1da3979548
venu7273 <109106057+venu7273@users.noreply.github.com>
2022-10-19 11:40:14 +0530

    Add files via upload

---
+++ sort.java
@@ -0,0 +1,40 @@
+import java.util.Arrays; 
+import java.util.Scanner; 
+    public class sort
+    {  
+    public static void main(String args[])   
+    {  
+  Scanner input = new Scanner(System.in);
+  Scanner a = new Scanner(System.in);
+try
+{
+    System.out.println("enter the no of countries");
+    int n=input.nextInt();
+    String[] countries= new String[n] ;
+ System.out.println("enter the countries");
+ for(int i = 0; i<n; i++) 
+{
+  countries[i]=a.nextLine(); 
+}
+    int size = n ;
+    for(int i = 0; i<size; i++)   
+    {  
+    for (int j = i+1; j<size; j++)   
+    {  
+      
+    if(countries[i].compareTo(countries[j])>0)   
+    {  
+    String temp = countries[i];  
+    countries[i] = countries[j];  
+    countries[j] = temp;  
+    }  
+    }  
+    }
+    System.out.println(Arrays.toString(countries));   
+ }
+    catch(Exception e)
+{ 
+System.out.println("Invalid");
+     }
+    }  
+    }
